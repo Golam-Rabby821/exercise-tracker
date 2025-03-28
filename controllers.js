8ae4bc516428716214d8755ab83355f122d7377a
Golam Rabby <golamrabby821@gmail.com>
2025-03-28 07:25:43 +0600

    "One model was enough"

--- controllers.js
+++ controllers.js
@@ -1,4 +1,4 @@
-const {Exercise, User, Log} = require('./models')
+const { User} = require('./models')
 
 const createUser = async (req, res) => {
     try {
@@ -48,26 +48,23 @@ const createExercise = async (req, res) => {
         }
 
         const formattedDate = date ? new Date(date) : new Date();
-       
-        const newExercise = new Exercise({
-            username: user.username,
+
+        user.log.push({
             description,
             duration: Number(duration),
             date: formattedDate
-        })
-        const savedExercise = await newExercise.save();
+        });
+        
+         // Save the updated user object
+         await user.save();
 
-        // Update logs
-        await updateUserLogs(user, savedExercise);
-
-        console.log(savedExercise)
-
-        res.status(201).json({ 
-            username: savedExercise.username,
-            description: savedExercise.description,
-            duration: savedExercise.duration,
-            date: savedExercise.date.toDateString(),
-            _id: savedExercise._id
+         // Return the user object with exercise fields
+         res.status(201).json({
+             username: user.username,
+             description,
+             duration: Number(duration),
+             date: formattedDate.toDateString(),
+             _id: user._id,
          });
 
     } catch (error) {
@@ -75,36 +72,6 @@ const createExercise = async (req, res) => {
         res.status(500).json({ error: 'Internal server error' });
     }
 }
-
-const updateUserLogs = async (user, savedExercise) => {
-    try {
-        let userLog = await Log.findOne({ username: user.username})
-
-        if(!userLog){
-            userLog = new Log({
-                username: user.username,
-                count: 0,
-                _id: user._id,
-                log: []
-            })
-        }
-        userLog.log.push({
-            description: savedExercise.description,
-            duration: savedExercise.duration,
-            date: savedExercise.date
-        })
-
-        userLog.count = userLog.log.length;
-
-        // Save the updated log
-        await userLog.save();
-
-    } catch (error) {
-        console.error('Error Updating Logs:', error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
-}
-
 
 const getLogs = async (req, res) => {
     try {
@@ -116,24 +83,36 @@ const getLogs = async (req, res) => {
         return res.status(404).json({ error: "User not found" })
     }
 
-    // Buidling a query buffer to filter acc to req params
-    let query = { username: user.username}
-    if(from) query.date = { $gte: new Date(from)};
-    if(to) query.date = {...query.date, $lte: new Date(to)};
+    let logs = user.log.map(exercise => ({
+      description: exercise.description,
+      duration: exercise.duration,
+      date: exercise.date.toDateString() // Format date as requested
+    }));
 
-    // Fetch exercises with optional limit
-    const logs = await Exercise.find(query).limit(parseInt(limit));
+    // Apply date filtering if from or to parameters exist
+    if (from) {
+      const fromDate = new Date(from);
+      logs = logs.filter(exercise => new Date(exercise.date) >= fromDate);
+    }
+    if (to) {
+      const toDate = new Date(to);
+      logs = logs.filter(exercise => new Date(exercise.date) <= toDate);
+    }
 
-    res.status(200).json({ 
-        username: user.username,
-        count: logs.length,
-        _id: user._id,
-        log: logs.map( log => ({
-            description: log.description,
-            duration: log.duration,
-            date: log.date.toDateString(),
-        }))
-     });
+    // Apply limit if provided
+    if (limit) {
+      logs = logs.slice(0, Number(limit));
+    }
+
+    // Construct response object
+    res.json({
+      username: user.username,
+      count: logs.length,
+      _id: user._id,
+      log: logs
+    });
+
+
     } catch (error) {
         console.error('Error Creating Exercises:', error);
         res.status(500).json({ error: 'Internal server error' });
--- models.js
+++ models.js
@@ -1,41 +1,21 @@
 const mongoose = require('mongoose')
 
-// Create schema
-const exerciseSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  duration: {
-    type: Number,
-    required: true
-  },
-  date: Date
-})
 
-const userSchema = new mongoose.Schema({username: {
+const userSchema = new mongoose.Schema({
+  username: {
     type: String,
     required: true,
     unique: true
-  }})
-
-const logSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  count: Number,
+  },
   log: [{
-    description: {type: String, required: true},
-    duration: {type: Number, required: true},
+    description: String,
+    duration: Number,
     date: Date
-  }]
+}]
 })
 
-// Models
-const Exercise = mongoose.model('Exercise', exerciseSchema);
-const User = mongoose.model('User', userSchema);
-const Log = mongoose.model('Log', logSchema);
 
-module.exports = {Exercise, User, Log}
+// Models
+const User = mongoose.model('User', userSchema);
+
+module.exports = { User}
